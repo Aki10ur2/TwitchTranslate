58f83f7076d0b853b4a582153f8b14a51150bf7d
Urua <24709418+UruaUru@users.noreply.github.com>
2021-11-28 23:52:38 +0900

    Add GAS translate_api

---
+++ TwitchTranslate/GoogleAppsScript/translate_api.js
@@ -0,0 +1,29 @@
+﻿function doGet(e) {
+
+    var p = e.parameter;
+    var translatedText = LanguageApp.translate(p.text, "", p.target);
+
+    // 今後の仕様で空文字返すかは考える
+    if (translatedText == p.text) {
+      translatedText = ""
+    }
+
+    var body;
+    if (translatedText) {
+        body = {
+          code: 200,
+          text: translatedText
+        };
+    } else {
+        body = {
+          code: 400,
+          text: "Bad Request"
+        };
+    }
+
+    var response = ContentService.createTextOutput();
+    response.setMimeType(ContentService.MimeType.JSON);
+    response.setContent(JSON.stringify(body));
+
+    return response;
+}
